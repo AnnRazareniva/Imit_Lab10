1bcd7885da6e019fba1d3bb3149e2e81ecbd3733
AnnRazareniva <145858547+AnnRazareniva@users.noreply.github.com>
2024-05-11 22:28:06 +0700

    Add files via upload

---
+++ 10/10/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace _10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label111 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(82, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Орлы";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(82, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Грифин";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(82, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Чивапчичи";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(82, 387);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "РиалСеверск";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.SystemColors.Control;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(246, 146);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(54, 17);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "label11";
+            this.label11.Visible = false;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.SystemColors.Control;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(246, 350);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 17);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "label12";
+            this.label12.Visible = false;
+            // 
+            // label111
+            // 
+            this.label111.AutoSize = true;
+            this.label111.BackColor = System.Drawing.SystemColors.Control;
+            this.label111.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label111.Location = new System.Drawing.Point(391, 247);
+            this.label111.Name = "label111";
+            this.label111.Size = new System.Drawing.Size(62, 17);
+            this.label111.TabIndex = 6;
+            this.label111.Text = "label111";
+            this.label111.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(477, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 121);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "МАТЧ!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(800, 633);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label111);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label111;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 10/10/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10
+{
+    public partial class Form1 : Form
+    {
+        double alfa;
+        Random rand = new Random();        
+        int[] rang = {1 ,2, 3, 5};
+        int[] score = { 0,0,0,0};
+        int match = 0;
+        int[] indexTeam = new int[2];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            match++;
+
+            for (int i = 0; i < rang.Length; i++) 
+            {
+                score[i] = Poisson(rang[i]);
+            }
+            
+            if (match < 2) 
+            {                    
+                
+                
+                if (score[0] > score[1])
+                {
+                    label11.Text = label1.Text;
+                    label11.Visible = true;
+                    
+                    indexTeam[0] = 0;
+                    label1.BackColor = Color.LawnGreen;
+
+                }
+                else 
+                {
+                    label11.Text = label2.Text;
+                    label11.Visible = true;
+                    //match++;
+                    indexTeam[0] = 1;
+                    label2.BackColor = Color.LawnGreen;
+                }
+
+                if (score[2] > score[3])
+                {
+                    label12.Text = label3.Text;
+                    label12.Visible = true;
+                    //match++;
+                    indexTeam[1] = 2;
+                    label3.BackColor = Color.LawnGreen;
+                }
+                else
+                {
+                    label12.Text = label4.Text;
+                    label12.Visible = true;
+                    //match++;
+                    indexTeam[1] = 3;
+                    label4.BackColor = Color.LawnGreen;
+                }
+                
+                label1.Text = label1.Text + "  " +  score[0].ToString();
+                label2.Text = label2.Text + "  " + score[1].ToString();
+                label3.Text = label3.Text + "  " + score[2].ToString();
+                label4.Text = label4.Text + "  " + score[3].ToString();
+            }
+
+            if(match == 2)
+            {
+                if (score[indexTeam[0]] > score[indexTeam[1]])
+                {
+                    label111.Text = label11.Text;
+                    label111.Visible = true;
+                    //match++;
+                    label11.BackColor = Color.LawnGreen;
+                }
+                else
+                {
+                    label111.Text = label12.Text;
+                    label111.Visible = true;
+                    //match++;
+                    label12.BackColor = Color.LawnGreen;
+                }
+
+                label11.Text = label11.Text + "  " + score[indexTeam[0]].ToString();
+                label12.Text = label12.Text + "  " + score[indexTeam[1]].ToString();
+
+            }
+        }
+        
+        
+        
+        public int Poisson(int rang)
+        {
+            double S = 0;
+            int m = 0;
+                while (S >= (-1* rang))
+                {
+
+                    alfa = rand.NextDouble();
+                    S += Math.Log(alfa);
+
+                    m += 1;
+                }
+            return m;
+        }
+
+
+    }
+
+
+    
+}
